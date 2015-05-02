30a2746f2476c0203ee8799a10b5e3137881cc13
Fanie Oosthuysen <fanie.oosthuysen@gmail.com>
2015-05-02 20:58:03 +0200

    Changes

--- calendar.js
+++ calendar.js
@@ -1,10 +1,13 @@
 var
   fs = require('fs')
   , promise = require('node-promise')
-	, EventEmitter = require('events').EventEmitter
-	, util = require('util')
+  , EventEmitter = require('events').EventEmitter
+  , util = require('util')
   , https = require('https')
-  , xml2js = require('xml2js')
+  , xpath = require('xpath')
+  , dom = require('xmldom').DOMParser
+  , soap = require('soap')
+  , path = require('path')
 ;
 
 var ExchangeCalendarList = function(opts) {
@@ -73,10 +76,9 @@ ExchangeCalendarList.prototype.request = function(opts) {
   url.headers = {
     'Content-Type': 'text/xml'
   };
-
+  
   opts.fields = opts.fields ? opts.fields : [
       "item:Subject",
-      "item:Body",
       "calendar:Location",
       "calendar:Start",
       "calendar:End",
@@ -85,82 +87,120 @@ ExchangeCalendarList.prototype.request = function(opts) {
 
   var fields = '';
   opts.fields.forEach(function(field) {
-    fields += '<t:FieldURI FieldURI="' + field + '"/>';
+    fields += '<t:FieldURI FieldURI="' + field + '"/>\n';
   });
-
-  var xml;
-
-  if (opts.start) {
-    var start = new Date(opts.start);
-    var end = new Date(opts.end);
   
-    start.toString = end.toString = function() {
-      return this.toISOString().replace(/\..+/, '') + '-00:00';
-    };
-
-    xml = self.templates.ews_find_items
-      .replace('$FolderId$', opts.folderid)
-      .replace('$StartDate$', start)
-      .replace('$EndDate$', end)
-    ;
-  }
-  else {
-    xml = self.templates.ews_find_items_all
-      .replace('$FolderId$', opts.folderid)
-      .replace('$MaxEntriesReturned$', opts.maximum)
-    ;
-  }
-
-  var first = promise.Promise();
-  first
-  .then(function() {
-    var p = promise.Promise();
-
-    var req = https.request(url, function(res) {
-      if (res.statusCode != 200) {
-        url.auth = 'username:password';
-        var html = '';
-        res.on('data', function(data) { html += data; });
-        res.on('end', function() {
-          p.reject(url.format() + ': ' + res.statusCode + '\n' + html);
-        });
-        return;
-      }
-      var data = '';
-      res.on('data', function(buffer) {
-        data += buffer;
-      });
-      res.on('end', function() {
-        p.resolve(data);
-      });
-    });
-    req.write(xml);
-    req.end();
-    return p;
-  })
-  .then(function(data) {
-    var p = promise.Promise();
-
-    xml2js.parseString(data, function(err, result) {
-      if (err) {
-        p.reject(err);
-        return;
-      }
-
-      // urg, I know
-      var items = result['s:Envelope']['s:Body'][0]['m:FindItemResponse'][0]['m:ResponseMessages'][0]['m:FindItemResponseMessage'][0]['m:RootFolder'][0]['t:Items'][0]['t:CalendarItem'];
-      items = items.map(function(item) {
-        return {
-          itemId: item['t:ItemId'][0]['$']['Id'],
-          changeKey: item['t:ItemId'][0]['$']['ChangeKey']
-        };
-      });
-      p.resolve(items);
-    });
+  var client = null;
+ 
+var first = promise.Promise();
+first
+.then(function(){
+	var p = promise.Promise();
+			
+	var endpoint = url.href;
+	var wsdl = path.join(__dirname, 'Services.wsdl');
+	
+	console.log(endpoint);
+	    
+	soap.createClient(wsdl, {}, function(err, clnt) {
+	  if (err) {
+	    return p.reject(err);
+	  }
+	  
+	  client = clnt;
+	  if (!client) {
+	    return p.reject('Could not create client');
+	  }
+      
+	  client.setSecurity(new soap.BasicAuthSecurity(opts.username, opts.password));
+	  p.resolve(client);
+	  return p;
+	}, endpoint);
+	
+	return p;
+})
+.then(function(client){
+	var p = promise.Promise();
+	
+	if (!opts.folderid) {
+		var xml = self.templates.ews_get_folder;
+		
+		client.GetFolder(xml, function(err, result, body) {
+			if (err) {
+			  return p.reject(err.message);
+			}
+			
+			var doc = new dom().parseFromString(body);
+			var select = xpath.useNamespaces({"t": "http://schemas.microsoft.com/exchange/services/2006/types"});
+			var nodes = select("//t:FolderId", doc);
+			if (nodes.length > 0) {
+			   p.resolve(nodes[0].getAttribute('Id'));
+			}
+			return p;
+		});
 
     return p;
-  })
-  .then(function(items) {
+	}
+	return opts.folderid;
+})
+.then(function(folderid) {
+	opts.folderid = folderid;
+	var xml;
+	if (opts.start) {
+	  var start = new Date(opts.start);
+	  var end = new Date(opts.end);
+	
+	  start.toString = end.toString = function() {
+	    return this.toISOString().replace(/\..+/, '') + '-00:00';
+	  };
+      
+	  xml = self.templates.ews_find_items
+	    .replace('$FolderId$', folderid)
+	    .replace('$StartDate$', start)
+	    .replace('$EndDate$', end)
+	    .replace('$FieldURI$', fields)
+	    .replace('$MaxEntriesReturned$', opts.maximum);
+	}
+	else {
+	  xml = self.templates.ews_find_items_all
+	    .replace('$FolderId$', folderid)
+	    .replace('$FieldURI$', fields)
+	    .replace('$MaxEntriesReturned$', opts.maximum);
+	}
+	
+	return xml;
+})
+.then(function(xml) {
+    var p = promise.Promise();
+    client.FindItem(xml, function(err, result, body) {
+			if (err) {
+			  return p.reject(err);
+			}
+			
+			p.resolve(body);
+			return p;
+		});
+    
+    return p;
+})
+.then(function(data) {
+	var p = promise.Promise();
+	    
+	var doc = new dom().parseFromString(data);
+	var select = xpath.useNamespaces({"t": "http://schemas.microsoft.com/exchange/services/2006/types"});
+	var nodes = select("//t:CalendarItem/t:ItemId", doc);
+	
+	items = nodes.map(function(item) {
+	   return {
+	     itemId: item.getAttribute('Id'),
+	     changeKey: item.getAttribute('ChangeKey')
+	  };
+	});
+	p.resolve(items);
+
+  return p;
+})
+.then(function(items) {
     var p = promise.Promise();
 
     var xml = self.templates.ews_get_item
@@ -168,66 +208,80 @@ ExchangeCalendarList.prototype.request = function(opts) {
       .replace('$FieldURI$', fields)
       .replace('$ItemId$', items.map(function(item) {
         return '<t:ItemId Id="' + item.itemId + '" ChangeKey="' + item.changeKey + '"/>'
-      }).join('\n'))
-    ;
-
-    var req = https.request(url, function(res) {
-      if (res.statusCode != 200) {
-        url.auth = 'username:password';
-        p.reject(url.format() + ': ' + res.statusCode);
-        res.on('data', function() {});
-        res.on('end', function() {});
-        return;
-      }
-      var data = '';
-      res.on('data', function(buffer) {
-        data += buffer;
-      });
-      res.on('end', function() {
-        p.resolve(data);
-      });
-    });
-    req.write(xml);
-    req.end();
+      }).join('\n'));
+    
+	  client.GetItem(xml, function(err, result, body) {
+				if (err) {
+					return p.reject(err);
+				}
+				
+				p.resolve(body);
+				return p;
+	  });
     return p;
-  })
-  .then(function(data) {
+})
+.then(function(data) {
     var p = promise.Promise();
+    
+    var doc = new dom().parseFromString(data);
+    var select = xpath.useNamespaces({"t": "http://schemas.microsoft.com/exchange/services/2006/types"});
+    var nodes = select("//t:CalendarItem", doc);
 
-    xml2js.parseString(data, function(err, result) {
-      if (err) {
-        p.reject(err);
-        return;
-      }
-
-      var items = result['s:Envelope']['s:Body'][0]['m:GetItemResponse'][0]['m:ResponseMessages'][0]['m:GetItemResponseMessage'];
-      //[0]['m:RootFolder'][0]['t:Items'][0]['t:CalendarItem'];
-      items = items.map(function(item) {
-        item = item['m:Items'][0]['t:CalendarItem'][0];
-        return {
-          itemId: item['t:ItemId'][0]['$']['Id'],
-          changeKey: item['t:ItemId'][0]['$']['ChangeKey'],
-          subject: item['t:Subject'][0],
-          location: item['t:Location'][0],
-          start: item['t:Start'][0],
-          end: item['t:End'][0],
-          categories: (item['t:Categories'] ? item['t:Categories'] : []).map(function(category) {
-            return category['t:String'][0];
-          }),
-          body: item['t:Body'][0]['_']
-        };
-      });
-      p.resolve(items);
+    items = nodes.map(function(item) {
+				return {
+					itemId: select('./t:ItemId', item)[0].getAttribute('Id'),
+					changeKey: select('./t:ItemId', item)[0].getAttribute('ChangeKey'),
+					type: firstData(select, './t:CalendarItemType', item),
+					subject: firstData(select, './t:Subject', item),
+					location: firstData(select, './t:Location', item),
+					start: firstData(select, './t:Start', item),
+					end: firstData(select, './t:End', item),
+					allDay: firstData(select, './t:IsAllDayEvent', item) == 'true' ? true : false,
+					body: firstData(select, './t:Body', item),
+					organizer: person(select, './t:Organizer', item)[0],
+					requiredAttendees: person(select, './t:RequiredAttendees/t:Attendee', item),
+					optionalAttendees: person(select, './t:OptionalAttendees/t:Attendee', item)
+				};
     });
-
-    return p;
-  })
-  .then(function(items) {
+   
+    return items;
+})
+.then(function(items) {
     p.resolve(items);
   },function(err) {
     p.reject(err);
-  });
-  first.resolve();
-
+});
+  
+	first.resolve();
   return p;
 };
+
+function person(select, query, node)
+{
+  if (node == null) {
+				return null;
+	}
+	
+	var ret = [];
+  var items = select(query, node);
+	for(var i = 0; i < items.length; i++)
+	{
+				var retItem = {};
+				var mb = select('./t:Mailbox/t:Name', items[i])[0];
+				if (mb && mb.firstChild) {
+				    retItem.name = mb.firstChild.data;
+				}
+				mb = select('./t:Mailbox/t:EmailAddress', items[i])[0];
+				if (mb && mb.firstChild) {
+				    retItem.email = mb.firstChild.data;
+				}
+				ret.push(retItem);
+	}
+	return ret;
+}
+
+function firstData(select, query, node)
+{
+  var item = select(query, node)[0];
+  return item && item.firstChild ? item.firstChild.data : null;
+}
